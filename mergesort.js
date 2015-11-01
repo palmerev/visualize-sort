24eb69a4174e26dab4767bb86f9444d91c2d2c16
Evan Palmer <evjpalmer@gmail.com>
2015-10-31 20:52:53 -0700

    add mergesort file and test spec file

---
+++ mergesort.js
@@ -0,0 +1,26 @@
+(function(window){
+    'use strict';
+
+    /**
+    * @param {array} list - An array to be sorted
+    * @returns {array} The sorted version of the array
+    */
+    function mergesort(list){
+
+    }
+
+    /**
+    * Takes two sorted arrays and returns one sorted array with all elements of
+    * listA and listB
+    *
+    * @param {array} listA - A sorted array
+    * @param {array} listB - A sorted array
+      @returns {array} The combined sorted array
+    */
+    function merge(listA, listB) {
+
+    }
+
+    window.mergesort = mergsort;
+    window.merge = merge;
+}(window));
