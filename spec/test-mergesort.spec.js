6169f8dcda209bfa2580f68ba6cd99ceeed19b77
Evan Palmer <evjpalmer@gmail.com>
2015-11-01 11:41:35 -0800

    add spec for merge function 'merges arrays of length 1'

--- spec/test-mergesort.spec.js
+++ spec/test-mergesort.spec.js
@@ -11,4 +11,12 @@ describe("merge function", function () {
     it("takes two arrays and returns an array", function () {
         expect(M.merge(arrayOne, arrayTwo)).toBe([]);
     });
+
+    it("merges arrays of length 1", function () {
+        var arr1 = arrayOne.concat([1]);
+        var arr2 = arrayTwo.concat([2]);
+        expect(arr1).toEqual([1]);
+        expect(arr2).toEqual([2]);
+        expect(M.merge(arr1, arr2)).toEqual([1, 2]);
+    });
 });
