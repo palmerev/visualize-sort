070e1f0c3109600f52c932543015a3677add1522
Evan Palmer <evjpalmer@gmail.com>
2015-11-03 20:21:55 -0800

    add specs for arrEqual function (passing)

---
+++ spec/test-arrequal.spec.js
@@ -0,0 +1,27 @@
+var MergeSort = require("../mergesort");
+
+describe("arrEqual", function () {
+    var M;
+    beforeEach(function () {
+        M = new MergeSort();
+    });
+
+    afterEach(function () {
+        M = null;
+    });
+
+    it("compares empty arrays", function (){
+        expect(M.arrEqual([],[])).toBe(true);
+    });
+
+    it("compares arrays with same members in same order", function () {
+        expect(M.arrEqual([1,2,3], [1,2,3])).toBe(true);
+    });
+
+    it("compares arrays with same members in different order", function () {
+        expect(M.arrEqual([1,2,3], [3,1,2])).toBe(false);
+    });
+    it("compares arrays of different lengths", function () {
+        expect(M.arrEqual([1,2,3], [1,2])).toBe(false);
+    });
+});
