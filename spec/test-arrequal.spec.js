2859d74eacd54146b50cdfe1a49d38e9e89a7fa0
Evan Palmer <evjpalmer@gmail.com>
2015-11-04 18:28:26 -0800

    add more specs for cases that give incorrect output in the browser

--- spec/test-arrequal.spec.js
+++ spec/test-arrequal.spec.js
@@ -17,12 +17,21 @@ describe("arrEqual", function () {
     });
 
     it("compares arrays with same members in same order", function () {
+        expect(M.arrEqual([0, 0, 1, 4, 9, 42], [0, 0, 1, 4, 9, 42])).toBe(true);
+    });
+
+    it("compares arrays with same members in same order", function () {
         expect(M.arrEqual([1, 2, 3], [1, 2, 3])).toBe(true);
     });
 
     it("compares arrays with same members in different order", function () {
         expect(M.arrEqual([1, 2, 3], [3, 1, 2])).toBe(false);
     });
+
+    it("compares arrays with same members with duplicates in different order", function () {
+        expect(M.arrEqual([0, 42, 0, 9, 1, 4], [0, 0, 1, 4, 9, 42])).toBe(false);
+    });
+
     it("compares arrays of different lengths", function () {
         expect(M.arrEqual([1, 2, 3], [1, 2])).toBe(false);
     });
--- spec/test-mergesort.spec.js
+++ spec/test-mergesort.spec.js
@@ -35,5 +35,17 @@ describe("mergesort:", function () {
             var sorted = M.mergesort(list);
             expect(M.arrEqual(sorted, [1, 2, 3, 4, 5])).toBe(true);
         });
+
+        it("sorts 0, 42, 0, 4, 9, 1 (even length with duplicates)", function () {
+            var list = arr.concat(0, 42, 0, 4, 9, 1);
+            var sorted = M.mergesort(list);
+            expect(M.arrEqual(sorted, [0, 0, 1, 4, 9, 42])).toBe(true);
+        });
+
+        it("sorts 1, 19, 15, 24, 20, 10, 100 (odd length varied numbers of digits)", function () {
+            var list = arr.concat(1, 19, 15, 24, 20, 10, 100);
+            var sorted = M.mergesort(list);
+            expect(M.arrEqual(sorted, [1, 10, 15, 19, 20, 24, 100])).toBe(true);
+        });
     });
 });
